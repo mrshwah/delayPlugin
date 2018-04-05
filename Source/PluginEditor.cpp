3b64996f6f770df909bbc83a92d3365171d3596e
Nick Chouard <nickchouard@MacBook-Pro.local>
2018-04-05 15:02:14 -0500

    Initial commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,125 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+DelayPlugInAudioProcessorEditor::DelayPlugInAudioProcessorEditor (DelayPlugInAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (600, 600);
+    
+    delaySlider.addListener(this);
+    delaySlider.setBounds(0,0,150,150);
+    delaySlider.setCentrePosition(100, 150);
+    delaySlider.setSliderStyle(Slider::RotaryVerticalDrag);
+    delaySlider.setTextBoxStyle(Slider::TextBoxBelow, false, 50, 30);
+    delaySlider.setRange(0, 1000, 1);
+    delaySlider.setValue(1000);
+    addAndMakeVisible(delaySlider);
+    
+    delayLabel.setFont(Font::Font("Avenir Next", 16.0f, 1));
+    delayLabel.setBounds(70, 125, 60, 16);
+    delayLabel.setText("TIME MS", dontSendNotification);
+    addAndMakeVisible(delayLabel);
+    
+    feedbackSlider.addListener(this);
+    feedbackSlider.setBounds(0,0,150,150);
+    feedbackSlider.setCentrePosition(300, 150);
+    feedbackSlider.setSliderStyle(Slider::RotaryVerticalDrag);
+    feedbackSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 50, 30);
+    feedbackSlider.setRange(0, 99, 1);
+    addAndMakeVisible(feedbackSlider);
+    
+    feedbackLabel.setFont(Font::Font("Avenir Next", 16.0f, 1));
+    feedbackLabel.setBounds(260, 125, 80, 16);
+    feedbackLabel.setText("FEEDBACK %", dontSendNotification);
+    addAndMakeVisible(feedbackLabel);
+    
+    highPassSlider.addListener(this);
+    highPassSlider.setBounds(0,0,150,150);
+    highPassSlider.setCentrePosition(100, 450);
+    highPassSlider.setSliderStyle(Slider::RotaryVerticalDrag);
+    highPassSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 50, 30);
+    highPassSlider.setRange(0, 1000, 1);
+    addAndMakeVisible(highPassSlider);
+    
+    highPassLabel.setFont(Font::Font("Avenir Next", 16.0f, 1));
+    highPassLabel.setBounds(75, 425, 60, 16);
+    highPassLabel.setText("HPF HZ", dontSendNotification);
+    addAndMakeVisible(highPassLabel);
+    
+    lowPassSlider.addListener(this);
+    lowPassSlider.setBounds(0,0,150,150);
+    lowPassSlider.setCentrePosition(300, 450);
+    lowPassSlider.setSliderStyle(Slider::RotaryVerticalDrag);
+    lowPassSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 50, 30);
+    lowPassSlider.setRange(2000, 20000, 1);
+    lowPassSlider.setValue(20000);
+    addAndMakeVisible(lowPassSlider);
+    
+    lowPassLabel.setFont(Font::Font("Avenir Next", 16.0f, 1));
+    lowPassLabel.setBounds(275, 425, 60, 16);
+    lowPassLabel.setText("LPF HZ", dontSendNotification);
+    addAndMakeVisible(lowPassLabel);
+    
+    wetDrySlider.addListener(this);
+    wetDrySlider.setBounds(0,0,150,150);
+    wetDrySlider.setCentrePosition(500, 300);
+    wetDrySlider.setSliderStyle(Slider::RotaryVerticalDrag);
+    wetDrySlider.setTextBoxStyle(Slider::TextBoxBelow, false, 50, 30);
+    wetDrySlider.setRange(0, 100, 1);
+    wetDrySlider.setValue(50);
+    addAndMakeVisible(wetDrySlider);
+    
+    wetDryLabel.setFont(Font::Font("Avenir Next", 16.0f, 1));
+    wetDryLabel.setBounds(460, 275, 80, 16);
+    wetDryLabel.setText("WET/DRY %", dontSendNotification);
+    addAndMakeVisible(wetDryLabel);
+}
+
+DelayPlugInAudioProcessorEditor::~DelayPlugInAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void DelayPlugInAudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setColour (Colours::white);
+    g.setFont(Font::Font("Avenir Next", 36.0f, 0));
+    g.drawFittedText("DELAY", 0, 50, 400, 36, Justification::centred, 1);
+    g.drawFittedText("FILTERS", 0, 350, 400, 36, Justification::centred, 1);
+    g.drawFittedText("OUTPUT", 400, 200, 200, 36, Justification::centred, 1);
+}
+
+void DelayPlugInAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
+
+void DelayPlugInAudioProcessorEditor::sliderValueChanged(Slider *slider)
+{
+    if (slider == &delaySlider) {
+        processor.delay = delaySlider.getValue() / 1000;
+    }
+    else if (slider == &feedbackSlider)
+        processor.feedback = .01 * feedbackSlider.getValue();
+    else if (slider == &wetDrySlider) {
+        processor.wetMix = wetDrySlider.getValue() / 100;
+    }
+}
---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,222 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+DelayPlugInAudioProcessor::DelayPlugInAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+DelayPlugInAudioProcessor::~DelayPlugInAudioProcessor()
+{
+    delay = 1000;
+    delaySmooth = 0.0f;
+    feedback = 0.0f;
+    delayBufferSize = 1;
+    delayReadPointer = 0;
+    delayWritePointer = 0;
+    wetMix = 0.5f;
+}
+
+//==============================================================================
+const String DelayPlugInAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool DelayPlugInAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool DelayPlugInAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool DelayPlugInAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double DelayPlugInAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int DelayPlugInAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int DelayPlugInAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void DelayPlugInAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String DelayPlugInAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void DelayPlugInAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void DelayPlugInAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    delayBufferSize = (int) (sampleRate);
+    
+    delayBuffer.setSize(getTotalNumInputChannels(), delayBufferSize);
+    delayBuffer.clear();
+}
+
+void DelayPlugInAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool DelayPlugInAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void DelayPlugInAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    ScopedNoDenormals noDenormals;
+    const int totalNumInputChannels  = getTotalNumInputChannels();
+    const int totalNumOutputChannels = getTotalNumOutputChannels();
+
+    int drp, dwp;
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (int i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        float* channelInput = buffer.getWritePointer(channel);
+        float* delaySamples = delayBuffer.getWritePointer(channel);
+        
+        drp = delayReadPointer;
+        dwp = delayWritePointer;
+        
+        for(int n = 0; n < buffer.getNumSamples(); ++n)
+        {
+            inSample = channelInput[n];
+            outSample = 0.0f;
+
+            delaySmooth = delay * 0.001 + delaySmooth*0.999;
+            
+            drp = (int) (dwp - (delaySmooth * getSampleRate()) + delayBufferSize) % delayBufferSize;
+            
+            outSample = (1 - wetMix) * inSample + wetMix * delaySamples[drp];
+            
+            delaySamples[dwp] = inSample + delaySamples[drp] * feedback;
+            
+            ++drp %= delayBufferSize;
+            ++dwp %= delayBufferSize;
+            
+            channelInput[n] = outSample;
+        }
+    }
+    
+    delayReadPointer = drp;
+    delayWritePointer = dwp;
+}
+
+//==============================================================================
+bool DelayPlugInAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* DelayPlugInAudioProcessor::createEditor()
+{
+    return new DelayPlugInAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void DelayPlugInAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void DelayPlugInAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new DelayPlugInAudioProcessor();
+}
